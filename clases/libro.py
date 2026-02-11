a702c1be46f2fc07349506688821cd2f1eacd2d1
Diego Ardila <diego.ardilas2486@gmail.com>
2026-02-11 13:57:30 -0500

    Se crea la clse libro den feat:libro

---
+++ clases/libro.py
@@ -0,0 +1,6 @@
+class Libro:
+    def __init__(self, titulo, autor, id, disponible=True):
+        self.titulo = titulo
+        self.autor = autor
+        self.id = id
+        self.disponible = disponible
