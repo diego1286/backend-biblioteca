a8fd345a951cc7b07d56d6297fddd27bc1a3da09
Diego Ardila <diego.ardilas2486@gmail.com>
2026-03-16 09:46:49 -0500

    Limpiar proyecto se eliminan folder src/entities y lo del primer examen

--- examen_miguel_carmona/src/Entidades/Sede.py
+++
@@ -1,7 +0,0 @@
-class Sede:
-    def __init__(self, nombre: str, direccion: str) -> None:
-        self._nombre = nombre
-        self._direccion = direccion
-
-    def obtener_nombre(self) -> str:
-        return self._nombre
